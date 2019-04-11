8323ca806975b3f82e55efd5d71d7996380f0fd6
Jane <janequichocho@gmail.com>
2019-04-11 04:15:41 -0700

    Passed Person tests

--- main.kts
+++ main.kts
@@ -33,17 +33,57 @@ fun add(num1: Int, num2: Int): Int {
 }
 
 // write a "sub" function that takes two Ints, returns an Int, and subtracts the values
-/*fun sub(Number: num1, Number: num2): Number {
+fun sub(num1: Int, num2: Int): Int {
     return num1 - num2
 }
 
 // write a "mathOp" function that takes two Ints and a function (that takes two Ints and returns an Int), returns an Int, and applies the passed-in-function to the arguments
 
-fun mathOp(Number: num1, Number: num2, mathFun: (Number: num3, Number: num4) -> Number) {
+fun mathOp(num1: Int, num2: Int, mathFun: (num3: Int, num4: Int) -> Int): Int {
     return mathFun(num1, num2)
-}*/
+}
 
 // write a class "Person" with first name, last name and age
+
+class Person {
+    var debugString: String = ""
+        get() = field
+
+    var firstName: String = ""
+        get() = field
+        set(value) {
+            field = value
+            this.updateDebugString()
+        }
+
+    var lastName: String = ""
+        get() = field
+        set(value) {
+            field = value
+            this.updateDebugString()
+        }
+
+    var age: Int = 0
+        get() = field
+        set(value) {
+            field = value
+            this.updateDebugString()
+        }
+
+    private fun updateDebugString() {
+        this.debugString = "[Person firstName:" + this.firstName + " lastName:" + this.lastName + " age:" + this.age + "]"
+    }
+
+    constructor(firstName: String, lastName: String, age: Int) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.age = age
+    }
+
+    fun equals(other: Person): Boolean {
+        return this.firstName == other.firstName && this.lastName == other.lastName && this.age == other.age;
+    }
+}
 
 // write a class "Money"
 
@@ -77,7 +117,7 @@ for ( (k,v) in add_tests) {
 }
 println("")
 
-/*print("Sub tests: ")
+print("Sub tests: ")
 val sub_tests = listOf(
     Pair(0, 0) to 0,
     Pair(2, 1) to 1,
@@ -103,7 +143,7 @@ p1.age = 48
 print(if (p1.debugString == "[Person firstName:Ted lastName:Neward age:48]") "." else "!")
 println("")
 
-print("Money tests: ")
+/*print("Money tests: ")
 val tenUSD = Money(10, "USD")
 val twelveUSD = Money(12, "USD")
 val fiveGBP = Money(5, "GBP")
